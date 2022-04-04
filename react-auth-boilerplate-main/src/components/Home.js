7d0488fb3f5fd62fc220428326d088aa27c46bba
jscodeguy <im.still.at.school99@gmail.com>
2022-04-04 17:05:55 -0400

    merge for client completed

--- react-auth-boilerplate-main/src/components/Home.js
+++ react-auth-boilerplate-main/src/components/Home.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-import IndexSightings from "./Sightings/IndexSightings"
-import ShowSightings from "./Sightings/ShowSightings"
->>>>>>> f0a37388ada89a9a3963384b47c300242fe4e6a9
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
@@ -10,13 +5,6 @@ const Home = (props) => {
 	return (
 		<>
 			<h2>Home Page</h2>
-<<<<<<< HEAD
-=======
-			<IndexSightings />
-			<ShowSightings />
-			<ShowFaves />
-	
->>>>>>> f0a37388ada89a9a3963384b47c300242fe4e6a9
 		</>
 	)
 }
