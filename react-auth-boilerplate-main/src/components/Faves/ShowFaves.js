701ac8e23b9758379a36392eca4d9bf41a5582c2
CameronASC20 <camerongarnier73@gmail.com>
2022-04-06 09:31:08 -0400

    Added Favorites Index Route

--- react-auth-boilerplate-main/src/components/Faves/ShowFaves.js
+++ react-auth-boilerplate-main/src/components/Faves/ShowFaves.js
@@ -1,28 +1,53 @@
 import React, {useState, useEffect} from 'react'
-import { getOneFave } from '../../api/faves'
+import { getOneFav } from '../../api/favorite'
 import { useParams } from 'react-router-dom'
+import { removeFav } from '../../api/favorite'
+import { Button } from 'react-bootstrap'
 
 
 const ShowFaves = (props) => {
 
-    const [fave, setFaves] = useState(null)
+    const {user, msgAlert, triggerRefresh} = props
+    const [favorites, setFaves] = useState(null)
     console.log('props in showFaves', props)
     const { id } = useParams()
     console.log('id in showFaves', id)
     // empty dependency array in useEffect to act like component did mount
     useEffect(() => {
-        getOneFave(id)
-            .then(res => setFaves(res.data.faves))
+        getOneFav(id)
+            .then(res => setFaves(res.data.favorites))
             .catch(console.error)
     }, [id])
 
-
+    const destroyFav = () => {
+        removeFav(user, favorites.id)
+            .then(() =>
+                msgAlert({
+                    heading: 'Fav updated!',
+                    message: 'great! the pet loves it!',
+                    variant: 'success',
+                }))
+            .then(() => triggerRefresh())
+            // if there is an error, we'll send an error message
+            .catch(() =>
+                msgAlert({
+                    heading: 'Oh No!',
+                    message: 'that aint it',
+                    variant: 'danger',
+            }))
+    }
 
     return (
         <>
-        <h5> show faves</h5>
-    </>
-)
+            <h5> Show Favorites</h5>
+            <p>{favorites.id}</p>
+            <p>{favorites.haveSeen}</p>
+            <p>{favorites.bird}</p>
+            <Button onClick={() => destroyFav()}variant="danger">
+                Delete
+            </Button>
+        </>
+    )
 }
 
 export default ShowFaves
