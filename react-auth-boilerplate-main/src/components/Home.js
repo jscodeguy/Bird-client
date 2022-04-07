b7a594d69180072160280ce88873eb32468af653
stephanieonyekaba <onyek009@umn.edu>
2022-04-07 15:45:19 -0400

    updates

--- react-auth-boilerplate-main/src/components/Home.js
+++ react-auth-boilerplate-main/src/components/Home.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 import IndexSightings from "./Sightings/IndexSightings"
 import ShowSightings from "./Sightings/ShowSighting"
 import ShowFaves from "./Faves/ShowFaves"
@@ -8,16 +7,16 @@ import IndexPhotos from "./Photos/IndexPhotos"
 import ShowPhotos from "./Photos/ShowPhoto"
 import {Container} from "react-bootstrap"
 
-=======
->>>>>>> cb59446877059eb3f278c1d2e86178f661cce3d7
 
 const Home = (props) => {
 	// const { msgAlert, user } = props
-	// console.log('props in home', props)
+
+
 
 	return (
 		<>
 		<Container className="home"> 
+
 		<h2>BirdBrains</h2>
 		<h3>A bird watching app for watchers who love birds</h3>
 		</Container>
--- react-auth-boilerplate-main/src/components/shared/Header.js
+++ react-auth-boilerplate-main/src/components/shared/Header.js
@@ -82,7 +82,9 @@ const Header = ({ user }) => (
 	<Navbar bg='primary' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
+
                 BirdBrains
+
             </Link>
         </Navbar.Brand>
 		<Navbar.Toggle aria-controls='basic-navbar-nav' />
