018b5fb40d99e339e0f84ac872860a1a22e8ff7e
CameronASC20 <camerongarnier73@gmail.com>
2022-04-07 14:28:32 -0400

    Added search bar form to Home.js

--- react-auth-boilerplate-main/src/components/Home.js
+++ react-auth-boilerplate-main/src/components/Home.js
@@ -1,14 +1,31 @@
 
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	// console.log('props in home', props)
+
+
+	const SearchBar = () => (
+		<form action="/" method="get">
+			<label htmlFor="header-search">
+				<span className="visually-hidden">Search</span>
+			</label>
+			<input
+				type="text"
+				id="header-search"
+				placeholder="Search"
+				name="s" 
+			/>
+			<button type="submit">Search</button>
+		</form>
+	);
 
 	return (
 		<>
 
 		<h2>BirdBrains</h2>
 		<h3>A bird watching app for watchers who love birds</h3>
-
+		<SearchBar />
 		</>
 	)
 }
