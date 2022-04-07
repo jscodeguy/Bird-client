c22cd0b6bae149568d5e0c51c42f406f8523e127
stephanieonyekaba <onyek009@umn.edu>
2022-04-07 16:48:48 -0400

    changes

--- react-auth-boilerplate-main/src/components/Home.js
+++ react-auth-boilerplate-main/src/components/Home.js
@@ -1,30 +1,11 @@
-<<<<<<< HEAD
-
-import IndexSightings from "./Sightings/IndexSightings"
-import ShowSightings from "./Sightings/ShowSighting"
-import ShowFaves from "./Faves/ShowFaves"
-import IndexFaves from "./Faves/IndexFaves"
-import IndexPhotos from "./Photos/IndexPhotos"
-import ShowPhotos from "./Photos/ShowPhoto"
-import {Container} from "react-bootstrap"
-=======
->>>>>>> StephanieBranch
 
 
 const Home = (props) => {
 	// const { msgAlert, user } = props
-<<<<<<< HEAD
-
-	return (
-		<>
-		<Container className="home"> 
-
-=======
 	// console.log('props in home', props)
 
 	return (
 		<>
->>>>>>> StephanieBranch
 
 		<h2>BirdBrains</h2>
 		<h3>A bird watching app for watchers who love birds</h3>
